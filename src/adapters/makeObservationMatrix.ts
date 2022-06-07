a476840f8665cec025bbe510d17100b00a9fb917
José Luis Pereira <pepelui@outlook.com>
2022-06-07 19:21:48 -0300

    Fix observation matrix adapter

--- src/adapters/makeObservationMatrix.ts
+++ src/adapters/makeObservationMatrix.ts
@@ -2,6 +2,6 @@ import { IObservationMatrix } from "../interfaces"
 
 export const makeObservationMatrix = (data: any): IObservationMatrix => ({
   observationMatrixId: data.observation_matrix_id,
-  name: data.name,
-  globalId: data.global_id
+  name: data.observation_matrix.name,
+  globalId: data.observation_matrix.global_id
 })
