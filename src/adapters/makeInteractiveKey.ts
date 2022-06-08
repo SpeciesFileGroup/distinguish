7e484f0d903ff5945cb444d86982637c1d43d7b4
José Luis Pereira <pepelui@outlook.com>
2022-06-07 21:07:52 -0300

    Remove unused code

--- src/adapters/makeInteractiveKey.ts
+++
@@ -1,8 +0,0 @@
-import { IInteractiveKey } from "../interfaces"
-
-export const makeInteractiveKey = (data: any): IInteractiveKey => ({
-  remaning: data.remaning,
-  eliminated: data.eliminated,
-  descriptorAvailableKeywords: data.descriptor_available_keywords,
-  descriptorAvailableLanguages: data.descriptor_available_languages,
-})
