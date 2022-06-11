f79227f5c0a0de729d85c4a83b397a9424fa8754
José Luis Pereira <pepelui@outlook.com>
2022-06-11 18:46:00 -0300

    Add createInteractiveKey and autodiscover initializers.

--- src/lib/main.ts
+++ src/lib/main.ts
@@ -1,6 +1,70 @@
-import '@/assets/stylesheets/main.scss'
+import { IAPIConfiguration } from '@/interfaces/IAPIConfiguration';
+import { createApp, App } from 'vue'
+import { createPinia } from 'pinia'
 import VueInteractiveKey from "../App.vue"
+import '@/assets/stylesheets/main.scss'
+
+function discoverInteractiveKey (selector?: string): Array<App> {
+  const tag: string = selector || '[data-interactive-key="true"]'
+  const elements: NodeListOf<HTMLElement> = document.querySelectorAll(tag)
+  const elementsList: Array<HTMLElement> = [...elements]
+
+  return elementsList.map(element => 
+    createInteractiveKey(element, parseElementOptions(element))
+  )
+}
+
+function autoDiscover (): void {
+  discoverInteractiveKey()
+}
+
+function parseElementOptions (element: HTMLElement): { observationMatrixId: number, apiConfig: IAPIConfiguration } {
+  return {
+    observationMatrixId: Number(element.getAttribute('data-observation-matrix-id')),
+    apiConfig: {
+      baseURL: element.getAttribute('data-api-url') || '',
+      projectToken: element.getAttribute('data-project-token') || undefined,
+      projectId: Number(element.getAttribute('data-project-id')) || undefined,
+      userToken: element.getAttribute('data-user-token') || undefined,
+      csrfToken: element.getAttribute('data-csrf-token') || undefined
+    }
+  }
+}
+
+function createInteractiveKey (element: HTMLElement | string, opt: { apiConfig: IAPIConfiguration, observationMatrixId: number }): App {
+  const {
+    apiConfig,
+    observationMatrixId
+  } = opt
+
+  if (!observationMatrixId) {
+    throw 'Missing observation matrix ID'
+  }
+
+  if (
+    !apiConfig ||
+    !apiConfig.projectToken &&
+    !apiConfig.csrfToken &&
+    !(apiConfig.projectId && apiConfig.userToken)
+  ) {
+    throw 'Missing API configuration'
+  }
+
+  const app = createApp(VueInteractiveKey, { 
+    observationMatrixId,
+    apiConfig
+  })
+
+  app.use(createPinia())
+  app.mount(element)
+
+  return app
+}
+
+window.addEventListener('DOMContentLoaded', autoDiscover)
 
 export { 
-  VueInteractiveKey 
+  VueInteractiveKey,
+  createInteractiveKey,
+  discoverInteractiveKey
 }
